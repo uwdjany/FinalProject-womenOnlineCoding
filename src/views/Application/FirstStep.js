ee5c74b897575765fcc33c46c09397319644c805
unknown <uwasey7@gmail.com>
2022-08-31 12:53:11 +0100

    gfc

--- src/views/Application/FirstStep.js
+++ src/views/Application/FirstStep.js
@@ -9,7 +9,7 @@ return(
 <h5 style={{marginLeft : '30%' ,fontSize:'20px',}}>PERSONAL INFORMATION</h5>
 <div>
 
-<TextField label="First name" margin="normal" variant="outlined" color="secondary" style={{width:'40%', marginLeft : '30%' ,marginTop: '50px'}} />
+<TextField label="First name" margin="normal" variant="outlined" color="secondary" style={{width:'41%', marginLeft : '30%' ,marginTop: '50px'}} />
 </div>
 <div>
 <TextField label="Last name" margin="normal" variant="outlined" color="secondary" style={{width:'40%', marginLeft : '30%'}} />
